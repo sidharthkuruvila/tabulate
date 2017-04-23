4c3a25ade87fc5a506565f70abfd83fe5e338ede
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2017-04-23 07:46:37 +0530

    Added check for missing header

--- tabulate.ml
+++ tabulate.ml
@@ -17,12 +17,11 @@ type column_hint = {
 }
 
 let draw_n n ch =
-  for i = 1 to n do
+  for _ = 1 to n do
     print_string ch
   done
 
-let draw_row index cell_fn data =
-  let border = borders.(index) in
+let draw_row border cell_fn data =
   let left = border.(0) in
   let center = border.(1) in
   let right = border.(2) in
@@ -44,31 +43,39 @@ let string_format s n =
 let draw_data_row column_hints row =
   let data = List.zip_exn column_hints row in
   let cell_fn ({width; _}, text) = print_string (string_format text width) in
-  draw_row 3 cell_fn data
+  draw_row borders.(3) cell_fn data
 
 let draw_separator index column_hints =
   let cell_fn {width; _} = draw_n width hline in
   draw_row index cell_fn column_hints
 
 let draw_table column_hints rows =
-  draw_separator 0 column_hints;
+  draw_separator borders.(0) column_hints;
   draw_data_row column_hints (List.map ~f:(fun {label; _} -> label) column_hints);
-  draw_separator 1 column_hints;
+  draw_separator borders.(1) column_hints;
   List.iter rows ~f:(fun row -> draw_data_row column_hints row);
-  draw_separator 2 column_hints
+  draw_separator borders.(2) column_hints
 
 let tabulate filename = 
-  let lines = In_channel.read_lines filename in
-  let lines = List.map ~f:(fun line -> String.split ~on:'\t' line) lines in
-  let header::rows = lines in
-  let columns = List.zip_exn header (List.transpose_exn rows) in 
-  let column_hints = List.map columns ~f:(fun (label, column) -> 
-    let width = Option.value_exn (List.max_elt (List.map ~f:String.length (label::column))
-        ~cmp:(fun a b -> a - b)) in
-    {label; width}
-  ) in
-  draw_table column_hints rows
-
+  In_channel.with_file filename ~f:(fun chan ->
+    let header_result = In_channel.input_line chan |> Result.of_option ~error:"Unable to parse header row" in
+    let prepare_table_result = Result.map header_result ~f:(fun header_line ->
+      let header = String.split ~on:'\t' header_line in 
+      let lines = In_channel.input_lines chan in
+      let rows = List.map ~f:(fun line -> String.split ~on:'\t' line) lines in  
+      let columns = List.zip_exn header (List.transpose_exn rows) in 
+      let column_hints = List.map columns ~f:(fun (label, column) -> 
+        let width = Option.value_exn (List.max_elt (List.map ~f:String.length (label::column))
+          ~cmp:(fun a b -> a - b)) in
+        {label; width}
+      ) in
+      (column_hints, rows)
+    ) in
+    match prepare_table_result with 
+      | Result.Ok (column_hints, rows) -> draw_table column_hints rows
+      | Result.Error msg -> Printf.eprintf "Failed to generate table: %s\n" msg
+  )
+  
 
 let spec =
   let open Command.Spec in
